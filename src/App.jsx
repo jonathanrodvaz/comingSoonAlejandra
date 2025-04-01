905f210f53e3e990e2e83f64583e114f7e77302d
Jonatan <jonathanrodvazquez@gmail.com>
2025-04-01 14:44:43 +0200

    Responsive Changes in the display of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,7 +11,7 @@ function App() {
      
       <h1>coming</h1>
       <h2>soon</h2>
-      
+
     </>
   )
 }
