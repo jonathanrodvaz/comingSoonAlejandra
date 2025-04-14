767cdd9dc5475eb8d649f5b556d714020edee405
Jonatan <jonathanrodvazquez@gmail.com>
2025-04-14 20:01:41 +0200

    Added more specific options for media queries

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import InstagramIcon from '/ig-icon.svg';
 
 
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 Alejandra Vacuii.</p>
+      <p>© 2025 Alejandra Vacuii</p>
+      <a href="https://www.instagram.com/lamalafortuna/" target="_blank" rel="noopener noreferrer">
+        <img 
+          src={InstagramIcon} 
+          alt="Instagram" 
+          className="instagram-icon" 
+        />
+      </a>
     </footer>
   );
 };
